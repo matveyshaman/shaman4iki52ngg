aa87cb25c1cfc64a7a7da3bad3e089d5cc4831ab
SavvaShaman4ik <143790399+SavvaShaman4ik@users.noreply.github.com>
2024-08-27 19:05:25 +0300

    nb

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Логика регистрации пользователя
+});
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Логика авторизации пользователя
+});
+
+document.getElementById('bookingForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Логика подачи заявки на бронирование
+});
